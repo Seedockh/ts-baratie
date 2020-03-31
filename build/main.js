6bf7a6a2906cf204014b78f253d503f5124f23c3
Pierre Hérissé <pierre.herisse@gmail.com>
2020-03-31 03:43:20 +0200

    ignore build folder

--- build/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const reception_1 = __importDefault(require("./src/reception"));
-const reception = new reception_1.default();
-reception.takeOrder();
-//# sourceMappingURL=main.js.map
--- build/src/kitchen.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=kitchen.js.map
